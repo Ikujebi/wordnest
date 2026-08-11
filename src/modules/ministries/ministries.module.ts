a0ea55cd790618793e85d25239d172b3cc4a8432
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2026-08-11 16:07:20 +0100

    Update ministries.module.ts

--- src/modules/ministries/ministries.module.ts
+++ src/modules/ministries/ministries.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
-import { MinistriesService } from './ministries.service';
-import { MinistriesController } from './ministries.controller';
-import { PrismaModule } from '../../prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { MinistriesService } from "./ministries.service";
+import { MinistriesController } from "./ministries.controller";
+import { PrismaModule } from "../../prisma/prisma.module";
+import { NotificationsModule } from "../notifications/notification.module";
+import { AuditLogModule } from "../audit-log/audit-log.module";
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [MinistriesController],
-  providers: [MinistriesService],
-  exports: [MinistriesService],
+  imports: [
+    PrismaModule,
+    NotificationsModule,
+    AuditLogModule,
+  ],
+
+  controllers: [
+    MinistriesController,
+  ],
+
+  providers: [
+    MinistriesService,
+  ],
+
+  exports: [
+    MinistriesService,
+  ],
 })
 export class MinistriesModule {}
+
